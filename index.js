286625f023c608fb3b2804c5965c4471a2235139
wacerakibucha <beatricekibucha@gmail.com>
2025-06-29 14:57:15 +0300

    adding fetch

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Add your code here
+function submitData(name, email) {
+  const formData = {
+    name: name,
+    email: email,
+  };
+
+  const configObj = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify(formData),
+  };
+
+  return fetch("http://localhost:3000/users", configObj)
+    .then((response) => response.json())
+    .then((data) => {
+      const p = document.createElement("p");
+      p.textContent = `User ID: ${data.id}`;
+      document.body.appendChild(p);
+    })
+    .catch((error) => {
+      const p = document.createElement("p");
+      p.textContent = error.message;
+      document.body.appendChild(p);
+    });
+}
